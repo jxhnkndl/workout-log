5eda4eb80d58a1ef623da6519d8bc41766708126
jxhnkndl <jkroyston@gmail.com>
2021-03-10 19:38:22 -0500

    Create HTML routes for index, adding a workout, and viewing workouts

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -5,6 +5,16 @@ const path = require('path');
 module.exports = (app) => {
   // Index route
   app.get('/', (req, res) => {
-    res.render('add_workout');
+    res.status(200).render('add_workout');
+  });
+
+  // Add workout page
+  app.get('/add_workout', (req, res) => {
+    res.status(200).render('add_workout');
+  });
+
+  // View workouts page
+  app.get('/view_workouts', (req, res) => {
+    res.status(200).render('view_workouts');
   });
 }

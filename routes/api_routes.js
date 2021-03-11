dd63c04d7f09c90a035fdef21bc7aa9749c8a675
jxhnkndl <jkroyston@gmail.com>
2021-03-10 20:35:36 -0500

    Add DELETE route for deleting workouts by id

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -15,11 +15,20 @@ module.exports = (app) => {
   app.post('/api/workouts', (req, res) => {
     const { date, category, distance, duration, details } = req.body;
 
-    db.Workout.create(req.body, (result) => {
+    db.Workout.create(req.body).then((dbWorkout) => {
       res.status(201).json({
         success: true,
-        id: result.insertId
+        id: dbWorkout.insertId,
       });
     });
+  });
+
+  // Delete workout
+  app.delete('/api/workouts/:id', (req, res) => {
+    db.Workout.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then((dbWorkout) => res.json(dbWorkout));
   });
 };

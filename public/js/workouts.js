9b6484b61f9501a78a3e593cd892b588b3034d4c
Kevin Spoth <kspoth08@su.edu>
2021-03-15 18:44:47 -0400

    adding patrials to view folder.

--- public/js/workouts.js
+++ public/js/workouts.js
@@ -1,13 +1,13 @@
-$(doument).ready(function () {
-  function viewWorkouts() {
+$(document).ready(function () {
+  $(".view-workout").on("click", (event) => {
+    event.preventDefault();
     console.log("View workouts clicked");
     $.ajax({
       url: "/api/workouts/",
       method: "GET",
       success: function (res) {
         console.log(res);
-        location.reload();
       },
     });
-  }
+  });
 });

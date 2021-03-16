4f76fb009752f95f90dcc8ed722d9d27cbf40433
jxhnkndl <jkroyston@gmail.com>
2021-03-16 16:19:49 -0400

    Reverse date returned by database for displaying in user interface

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,4 +1,5 @@
 // Import modules
+const { reverse } = require('dns');
 const path = require('path');
 const db = require('../models');
 
@@ -17,7 +18,22 @@ module.exports = (app) => {
   // View workouts page -> Render view_workouts with data from db
   app.get('/view_workouts', async (req, res) => {
     try {
-      const data = await db.Workout.findAll({ raw: true });
+      const data = await db.Workout.findAll({ 
+        raw: true, 
+        order: [
+          ['id', 'DESC']
+      ]});
+
+      // Reverse date for UI
+      data.forEach((workout) => {
+        let reverseDate = workout.workout_date;
+        
+        const year = reverseDate.slice(0, 4);
+        const month = reverseDate.slice(5, 7);
+        const day = reverseDate.slice(8, 10);
+
+        workout.workout_date = `${month}/${day}/${year}`;
+      });
 
       // Format returned data to pass into Handlebars
       const workouts = {
